042ebc748ec4d9b017383f127e814b3470ffc244
Alfonso Socarras <asocarras10@gmail.com>
2019-09-02 16:00:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?"
   new_sentence = sentence1.split(" ")
   new_sentence.each do |element|
-    element.reverse 
+    new_sentence.reverse 
   end
   new_sentence
 end
