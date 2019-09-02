9e4d92f6e6fcdc3a243d285ad8ab5d6dd3e06c0a
Alfonso Socarras <asocarras10@gmail.com>
2019-09-02 15:54:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-  string = "Hello there, and how are you?"
-  string.split
-  p string(" ")
+def reverse_each_word(String)
+  String = "Hello there, and how are you?"
+  String.split("")
+  p string
 end
